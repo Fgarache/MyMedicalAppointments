bb290e3328512b2a7725e96add28c1b4f481a49a
Francisco Garache <franciscogarach3@gmail.com>
2022-02-14 12:52:12 -0600

    Initial commit

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,17 +1,25 @@
 public class Doctor {
     static int id=0;
-    String name, speciality;
+    String name, speciality, email;
 
     Doctor (){
-        System.out.println("metodo constructor   "+name);
-        id++;
- }
+       System.out.println("El nombre asignado es   "+name);
+    }
 
+    Doctor (String name, String speciality){
+        System.out.println("nombre "+name);
+        id++;
+        this.name = name ;
+        this.speciality = speciality;
+
+    }
+
+    //comportamientos
     public void showName(){
-        System.out.println(name);
+       // System.out.println(name);
     }
 
     public void showId (){
-        System.out.println(id);
+        //System.out.println(id);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,17 @@ import static  ui.UIMenu.*;
 
 public class Main {
     public static void main(String[] args) {
-        showMenu();
+        //showMenu();
+        Doctor myDoctor = new Doctor("Anahi Salgado", "Pediatria");
+        System.out.println(myDoctor.name + "\n" + myDoctor.speciality);
+
+        Patient patient = new Patient("Alejandra", "Alejandra@gmail.com");
+
+        patient.setWeight(54.6);
+        System.out.println(patient.getWeight());
+        patient.setPhoneNumber("89224421");
+        System.out.println(patient.getPhoneNumber());
+
+
     }
 }
